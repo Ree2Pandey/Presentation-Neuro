c36bea4b77472dda14c59b12593fcb975cbe8a31
Ritu Pandey <ug2184tm@mnstate.edu>
2019-10-03 23:34:07 -0500

    New Graph for Neurobiology

--- New Graph.R
+++ New Graph.R
@@ -27,23 +27,31 @@ d <-
   mutate(genotype = as_factor(genotype)) %>% 
   print()
 
+
+
+
 # take a quick look at all the variables in the dataset
 glimpse(d)
 
 distinct(d, genotype)
 
+ 
+
 
 # Compare V1 area between the treatments --------------------------------
 
-ggplot(d, aes(x=genotype, y=visual, color = genotype)) +
+ggplot(d, aes(x= genotype, y= visual, color = genotype)) +
   geom_point(size = 4) +
   labs(
     x="Area (mm2) of the Primary Visual Area",
     y=NULL
   ) +
-  scale_y_discrete(limits = rev(levels(d$genotype))) +  # put genotype in reverse order
-  guides(color = "none") +
+   guides(color = "none") +
   theme_gray(base_size = 16)                            # make text bigger; default was 11
+
+ggsave("figs/compare Visual Area between the treatments.png", width = 11.5, height = 4.76, units = "in")
+
+
 
 ggsave("figs/compare Visual Area between the treatments.png", width = 11.5, height = 4.76, units = "in")
 
@@ -51,17 +59,17 @@ ggsave("figs/compare Visual Area between the treatments.png", width = 11.5, heig
 
 
 
+
 # Compare Visual area by Cortex ratio between the treatments -----------------
 
-ggplot(d, aes(x=genotype, y=ratiovc, color = genotype)) +
+ggplot(d, aes(x= genotype, y= ratiovc, color = genotype)) +
   geom_point(size = 4) +
   labs(
     x="Proportion of the Neocortex occupied by the Primary Visual Area",
     y=NULL
   ) +
-  scale_y_discrete(limits = rev(levels(d$genotype))) +  # put genotype in reverse order
   guides(color = "none") +
-  theme_gray(base_size =16)                       # make text bigger; default was 11
+  theme_gray(base_size = 16)                            # make text bigger; default was 11
 
 ggsave("figs/Compare Visual Area by Cortex ratio between the treatments.png", width = 11.5, height = 4.76, units = "in")
 
@@ -70,13 +78,12 @@ ggsave("figs/Compare Visual Area by Cortex ratio between the treatments.png", wi
 
 # Compare Motor Area between the treatments-------------------------------------------------------------
 
-ggplot(d, aes(x=genotype, y=motor, color = genotype)) +
+ggplot(d, aes(x= genotype, y= motor, color = genotype)) +
   geom_point(size = 4) +
   labs(
     x="Area (mm2) of the Primary Motor Area",
     y=NULL
   ) +
-  scale_y_discrete(limits = rev(levels(d$genotype))) +  # put genotype in reverse order
   guides(color = "none") +
   theme_gray(base_size = 16)                            # make text bigger; default was 11
 
@@ -88,17 +95,23 @@ ggsave("figs/Compare Motor Area between the treatments.png", width = 11.5, heigh
 
 # Compare Motor area by Cortex ratio between the treatments -------------------------------------------------------------
 
-ggplot(d, aes(x=genotype, y=ratiom, color = genotype)) +
+ggplot(d, aes(x= genotype, y=ratiom, color = genotype)) +
   geom_point(size = 4) +
   labs(
     x="Proportion of the Neocortex occupied by the Primary Motor Area",
     y=NULL
   ) +
-  scale_y_discrete(limits = rev(levels(d$genotype))) +  # put genotype in reverse order
   guides(color = "none") +
   theme_gray(base_size = 16)                            # make text bigger; default was 11
 
 ggsave("figs/Compare Motor Area by Cortex ratio between the treatmentss.png", width = 11.5, height = 4.76, units = "in")
+
+
+
+
+
+
+
 
 
 
