3f1fdc207070c69c3c5e897b485345cdac4a42f8
Ritu Pandey <ug2184tm@mnstate.edu>
2019-10-04 11:48:28 -0500

    Final Graphs

--- Graphs.R
+++ Graphs.R
@@ -4,11 +4,6 @@
 # check if tidyverse is installed and if not then install it
 if (!require("tidyverse")) install.packages("tidyverse")
 
-# load packages -----------------------------------------------------------
-
-# check if tidyverse is installed and if not then install it
-if (!require("tidyverse")) install.packages("tidyverse")
-
 # run every time you restart RStudio
 library(readxl)     # load readxl, for reading Excel files
 library(tidyverse)  # load tidyverse, for working with datasets
@@ -19,15 +14,9 @@ library(tidyverse)  # load tidyverse, for working with datasets
 # read the Presentation data
 d <- 
   read_excel(
-<<<<<<< HEAD
     "Book1.xlsx", 
     col_names = c("genotype", "visual", "cortex", "ratiovc", "posmed", "ratiopm", "area", "ratioarea", "motor", "ratiom"),
     skip = 3
-=======
-    "Double-Emx-KO_DATASET.xlsx", 
-   col_names = c("genotype", "visual", "cortex", "ratiovc", "posmed", "ratiopm", "area", "ratioarea", "motor", "ratiom"),
-  skip = 3
->>>>>>> 0129cb5ec59bbdacee66b23a7b59d67b09e4d261
   ) %>% 
   select(genotype, visual, ratiovc, motor, ratiom) %>% 
   filter(!str_detect(genotype, "3 allele")) %>% 
@@ -39,6 +28,18 @@ glimpse(d)
 
 distinct(d, genotype)
 
+
+# summarize data ----------------------------------------------------------
+
+d_mean <-
+  d %>% 
+  group_by(genotype) %>% 
+  summarize(
+    visual = mean(visual, na.rm = TRUE),
+    ratiovc = mean(ratiovc, na.rm = TRUE),
+    motor = mean(motor, na.rm = TRUE),
+    ratiom = mean(ratiom, na.rm = TRUE)
+  )
 
 # Compare V1 area between the treatments --------------------------------
 
@@ -60,10 +61,13 @@ ggsave("figs/Compare V1 area between the treatments.png", width = 11.5, height =
 # Compare Visual area by Cortex ratio between the treatments -----------------
 
 ggplot(d, aes(x= genotype, y= ratiovc, color = genotype)) +
+  geom_errorbar(mapping = aes(ymin = ratiovc, ymax = ratiovc), 
+                data = d_mean, color = "black", width = .5, size = 2) +
   geom_point(size = 4) +
   labs(
-    x="Proportion of the Neocortex occupied by the Primary Visual Area",
-    y=NULL
+    title="Proportion of the Neocortex occupied by the Primary Visual Area",
+    y = "Proportion",
+    x=NULL
   ) +
   guides(color = "none") +
   theme_gray(base_size = 16)                            # make text bigger; default was 11
