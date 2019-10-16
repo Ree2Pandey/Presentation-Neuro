4c0995e7c84258a061ea7824df96635235a364fa
Ritu Pandey <ug2184tm@mnstate.edu>
2019-10-16 03:11:25 -0500

    Neezu's Graph before Adam's feedback.

--- Neezu.R
+++ Neezu.R
@@ -13,11 +13,11 @@ library(tidyverse)  # load tidyverse, for working with datasets
 # read the Presentation data
 d <- 
   read_excel(
-    "Neezu's.xlsx", 
+    "Neezu excel.xlsx", 
     col_names = c("genotype", "visual", "cortex", "ratiovc", "posmed", "ratiopm", "area", "ratioarea", "motor", "ratiom"),
     skip = 3
   ) %>% 
-  select(genotype, cortex, ratiovc, ratiom, ) %>% 
+  select(genotype, cortex, ratiovc, ratiom) %>% 
   mutate(genotype = as_factor(genotype)) %>% 
   print()
 
@@ -33,27 +33,28 @@ d_mean <-
   d %>% 
   group_by(genotype) %>% 
   summarize(
-    visual = mean(visual, na.rm = TRUE),
+    cortex = mean(cortex, na.rm = TRUE),
     ratiovc = mean(ratiovc, na.rm = TRUE),
-    motor = mean(motor, na.rm = TRUE),
     ratiom = mean(ratiom, na.rm = TRUE)
   )
 
-# Compare V1 area between the treatments --------------------------------
+# Compare cortex area between the treatments --------------------------------
 
-ggplot(d, aes(x= genotype, y= visual, color = genotype)) +
-  geom_errorbar(mapping = aes(ymin = visual, ymax = visual), 
+ggplot(d, aes(x= genotype, y= cortex, color = genotype)) +
+  geom_errorbar(mapping = aes(ymin = cortex, ymax = cortex), 
                 data = d_mean, color = "black", width = .5, size = 2) +
   geom_point(size = 4) +
   labs(
-    title="Area of the Primary Visual Area",
+    title="Area of the Dorsal Neocortex",
     y="Area (mm2)",
     x="Genotype"
   ) +
   guides(color = "none") +
   theme_gray(base_size = 16)                            # make text bigger; default was 11
 
-ggsave("figs/Compare V1 area between the treatments.png", width = 11.5, height = 4.76, units = "in")
+ggsave("figs/Compare cortex area between the treatments.png", width = 11.5, height = 4.76, units = "in")
+
+
 
 
 
@@ -77,24 +78,6 @@ ggsave("figs/Compare Visual area by Cortex ratio between the treatments.png", wi
 
 
 
-
-# Compare Motor Area between the treatments-------------------------------------------------------------
-
-
-ggplot(d, aes(x= genotype, y= motor, color = genotype)) +
-  geom_errorbar(mapping = aes(ymin = motor, ymax = motor), 
-                data = d_mean, color = "black", width = .5, size = 2) +
-  geom_point(size = 4) +
-  labs(
-    title="Area of the Primary Motor Area",
-    y="Area (mm2)",
-    x="Genotype"
-  ) +
-  guides(color = "none") +
-  theme_gray(base_size = 16)                            # make text bigger; default was 11
-
-
-ggsave("figs/Compare Motor Area between the treatments.png", width = 11.5, height = 4.76, units = "in")
 
 
 
