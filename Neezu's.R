f5bc5ad78ba3517fa80393f5476a07b99a562949
Ritu Pandey <ug2184tm@mnstate.edu>
2019-10-16 02:54:41 -0500

    Neezu's Excel sheet

---
+++ Neezu's.R
@@ -0,0 +1,129 @@
+# load packages -----------------------------------------------------------
+
+# check if tidyverse is installed and if not then install it
+if (!require("tidyverse")) install.packages("tidyverse")
+
+# run every time you restart RStudio
+library(readxl)     # load readxl, for reading Excel files
+library(tidyverse)  # load tidyverse, for working with datasets
+
+
+# read data ---------------------------------------------------------------
+
+# read the Presentation data
+d <- 
+  read_excel(
+    "SFN Excel new.xlsx", 
+    col_names = c("genotype", "visual", "cortex", "ratiovc", "posmed", "ratiopm", "area", "ratioarea", "motor", "ratiom"),
+    skip = 3
+  ) %>% 
+  select(genotype, visual, ratiovc, motor, ratiom) %>% 
+  mutate(genotype = as_factor(genotype)) %>% 
+  print()
+
+# take a quick look at all the variables in the dataset
+glimpse(d)
+
+distinct(d, genotype)
+
+
+# summarize data ----------------------------------------------------------
+
+d_mean <-
+  d %>% 
+  group_by(genotype) %>% 
+  summarize(
+    visual = mean(visual, na.rm = TRUE),
+    ratiovc = mean(ratiovc, na.rm = TRUE),
+    motor = mean(motor, na.rm = TRUE),
+    ratiom = mean(ratiom, na.rm = TRUE)
+  )
+
+# Compare V1 area between the treatments --------------------------------
+
+ggplot(d, aes(x= genotype, y= visual, color = genotype)) +
+  geom_errorbar(mapping = aes(ymin = visual, ymax = visual), 
+                data = d_mean, color = "black", width = .5, size = 2) +
+  geom_point(size = 4) +
+  labs(
+    title="Area of the Primary Visual Area",
+    y="Area (mm2)",
+    x="Genotype"
+  ) +
+  guides(color = "none") +
+  theme_gray(base_size = 16)                            # make text bigger; default was 11
+
+ggsave("figs/Compare V1 area between the treatments.png", width = 11.5, height = 4.76, units = "in")
+
+
+
+
+
+# Compare Visual area by Cortex ratio between the treatments -----------------
+
+ggplot(d, aes(x= genotype, y= ratiovc, color = genotype)) +
+  geom_errorbar(mapping = aes(ymin = ratiovc, ymax = ratiovc), 
+                data = d_mean, color = "black", width = .5, size = 2) +
+  geom_point(size = 4) +
+  labs(
+    title="Proportion of the Neocortex occupied by the Primary Visual Area",
+    y = "Proportion",
+    x = "Genotype"
+  ) +
+  guides(color = "none") +
+  theme_gray(base_size = 16)                            # make text bigger; default was 11
+
+ggsave("figs/Compare Visual area by Cortex ratio between the treatments.png", width = 11.5, height = 4.76, units = "in")
+
+
+
+
+# Compare Motor Area between the treatments-------------------------------------------------------------
+
+
+ggplot(d, aes(x= genotype, y= motor, color = genotype)) +
+  geom_errorbar(mapping = aes(ymin = motor, ymax = motor), 
+                data = d_mean, color = "black", width = .5, size = 2) +
+  geom_point(size = 4) +
+  labs(
+    title="Area of the Primary Motor Area",
+    y="Area (mm2)",
+    x="Genotype"
+  ) +
+  guides(color = "none") +
+  theme_gray(base_size = 16)                            # make text bigger; default was 11
+
+
+ggsave("figs/Compare Motor Area between the treatments.png", width = 11.5, height = 4.76, units = "in")
+
+
+
+
+
+# Compare Motor area by Cortex ratio between the treatments -------------------------------------------------------------
+
+
+ggplot(d, aes(x= genotype, y= ratiom, color = genotype)) +
+  geom_errorbar(mapping = aes(ymin = ratiom, ymax = ratiom), 
+                data = d_mean, color = "black", width = .5, size = 2) +
+  geom_point(size = 4) +
+  labs(
+    title="Proportion of the Neocortex occupied by the Primary Motor Area",
+    y = "Proportion",
+    x = "Genotype"
+  ) +
+  guides(color = "none") +
+  theme_gray(base_size = 16)                            # make text bigger; default was 11
+
+ggsave("figs/Compare Motor Area by Cortex ratio between the treatmentss.png", width = 11.5, height = 4.76, units = "in")
+
+
+
+
+
+
+
+
+
+
+
